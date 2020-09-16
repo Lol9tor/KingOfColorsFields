a54173fe34d70ff4ad1de3a7b204c3c91c4d0a1c
Oleh <lol9tor@gmail.com>
2020-09-16 22:54:28 +0300

    Update dependencies and run project

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,45 @@
-const { resolve } = require('path');
-
+const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const env = process.env.NODE_ENV;
 
 const config = {
-	devtool: 'cheap-module-eval-source-map',
-
 	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./index.js',
-		'./assets/scss/index.scss',
+		'./src/index.js',
+		'./src/assets/scss/index.scss',
 	],
 
 	output: {
-		filename: 'bundle.js',
-		path: resolve(__dirname, 'dist'),
-		publicPath: '/',
-	},
-
-	context: resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
 
 	devServer: {
-		hot: true,
-		// contentBase: resolve(__dirname, 'dist'),
-		publicPath: '/dist',
+		contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3030,
+    hot: true,
 	},
 
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
-				loaders: [
-					'babel-loader',
-				],
-				exclude: /node_modules/,
-			},
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
 			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						'css-loader',
-						{
-							loader: 'sass-loader',
-							query: {
-								sourceMap: false,
-							},
-						},
-					],
-				}),
-			},
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      },
 			{ test: /\.(png|jpg)$/, use: 'url-loader?limit=15000' },
 			{ test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
 			{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
@@ -63,10 +49,14 @@ const config = {
 	},
 
 	plugins: [
-		new ExtractTextPlugin({ filename: 'index.css', disable: false, allChunks: true }),
-		// new CopyWebpackPlugin([{ from: 'vendors', to: 'vendors' }]),
-		new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+		new HtmlWebpackPlugin({template: './index.html'}),
 		new webpack.HotModuleReplacementPlugin(),
+		new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
 	],
 };
 
